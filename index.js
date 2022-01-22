51bd4b8379fe76d18629ddc5ac39d6c10f2a119a
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-21 19:43:18 -0500

    Route basics

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const express = require("express");
 
 const app = express();
 
+const productos = [
+  { producto: "Libro", precio: 20 },
+  { producto: "Computadora", precio: 10000 },
+];
+
 //  Ruta para el home
 app.use("/", (req, res, next) => {
-  res.send("Hola");
+  res.json(productos);
 });
 
 app.listen(3000);
