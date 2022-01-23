eac1172eea3b7a577eb1978fad99634cc43c21f3
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-22 21:46:54 -0500

    Generating an URL to the each project

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,4 +1,5 @@
 const Proyectos = require("../models/Proyectos.js");
+const slug = require("slug");
 
 exports.proyectosHome = (req, res) => {
   res.render("index", {
@@ -31,7 +32,8 @@ exports.nuevoProyecto = async (req, res) => {
     //No hay errores
     //Insertar en la BD
     try {
-      const proyecto = await Proyectos.create({ nombre });
+      const url = slug(nombre).toLowerCase();
+      const proyecto = await Proyectos.create({ nombre, url });
       res.redirect("/");
     } catch (err) {
       console.log(err);
