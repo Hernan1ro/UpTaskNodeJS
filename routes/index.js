a6c1c6117dea7d927383cadf1efae96b9fa03ddb
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-22 21:13:53 -0500

    adding express validator to validate data from req.body

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+// Importar express validator
+const { body } = require("express-validator/check");
+
 // Importar el controlador
 const proyectoController = require("../controllers/proyectosController.js");
 
 module.exports = function () {
   router.get("/", proyectoController.proyectosHome);
   router.get("/nuevo-proyecto", proyectoController.formularioProyecto);
-  router.post("/nuevo-proyecto", proyectoController.nuevoProyecto);
+  router.post(
+    "/nuevo-proyecto",
+    body("nombre").not().isEmpty().trim().escape(),
+    proyectoController.nuevoProyecto
+  );
 
   return router;
 };
